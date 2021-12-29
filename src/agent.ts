eb611fa928b28fde7e60fe8ec0998594aa3e2214
RodroVMS <democlesvanshepars@gmail.com>
2021-12-29 16:39:43 -0500

    Update agent to track affected address, contracts and amount

--- src/agent.ts
+++ src/agent.ts
@@ -11,12 +11,13 @@ import {
   TIMES_DETECTED,
 } from "./const";
 import EXCHANGES from "./exchanges";
-import { phishingAlert, isZeroAddress } from "./utils";
+import { phishingAlert, isZeroAddress, SpenderActivity } from "./utils";
+import { BigNumber } from "ethers";
 
 const provider = getEthersProvider();
 
 function provideHandleTransaction(provider: ethers.providers.JsonRpcProvider) {
-  const suspiciousSpenders: Map<string, number[]> = new Map();
+  const suspiciousSpenders: Map<string, SpenderActivity[]> = new Map();
   let lastBlockNumber: number = 0;
 
   return async (txEvent: TransactionEvent) => {
@@ -33,29 +34,50 @@ function provideHandleTransaction(provider: ethers.providers.JsonRpcProvider) {
       console.log(`got logs(${logs.length}):`, logs[0].signature);
 
     for (const log of logs) {
-      const spender = log.args.spender;
+      const spender: string = log.args.spender;
+      const amount: BigNumber = log.args.amount;
       // Do not analyze transactions from known exchanges and smart contracts
       if (
         EXCHANGES.has(spender) ||
         (await provider.getCode(spender, txEvent.blockNumber)) !== "0x" ||
-        isZeroAddress(spender)
+        isZeroAddress(spender) ||
+        amount.gt(0)
       ) {
         console.log("log belongs to contract or exchange");
         continue;
       }
+
+      const owner: string = txEvent.from;
+      const contractAddress = txEvent.to;
 
       let transactions = suspiciousSpenders.get(spender);
       // Block number where suspicious activity ocurred are kept
       // for the time specified.
       if (transactions === undefined) {
         console.log(`for ${spender} adding activity 1`);
-        transactions = [txEvent.blockNumber];
+        transactions = [
+          new SpenderActivity(
+            txEvent.blockNumber,
+            txEvent.hash,
+            owner,
+            contractAddress !== null ? contractAddress : "0x",
+            amount
+          ),
+        ];
         suspiciousSpenders.set(spender, transactions);
       } else {
         console.log(
           `for ${spender} adding activity ${transactions.length + 1}`
         );
-        transactions.push(txEvent.blockNumber);
+        transactions.push(
+          new SpenderActivity(
+            txEvent.blockNumber,
+            txEvent.hash,
+            owner,
+            contractAddress !== null ? contractAddress : "0x",
+            amount
+          )
+        );
       }
 
       if (transactions.length >= TIMES_DETECTED) {
@@ -79,11 +101,11 @@ function provideHandleTransaction(provider: ethers.providers.JsonRpcProvider) {
 // Delete every spender's suspicious activity which occurred
 // BLOCK_RANGE + 1 blocks ago
 function updateSuspicious(
-  suspiciousSpenders: Map<string, number[]>,
+  suspiciousSpenders: Map<string, SpenderActivity[]>,
   blockNumber: number
 ) {
   suspiciousSpenders.forEach((blocks, spender) => {
-    while (blockNumber - blocks[0] > BLOCK_RANGE) {
+    while (blockNumber - blocks[0].blockNumber > BLOCK_RANGE) {
       blocks.shift();
       if (blocks.length === 0) {
         suspiciousSpenders.delete(spender);
