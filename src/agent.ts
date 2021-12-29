f2e57a9ba3257d331fb28f9e2bcd725965af919f
RodroVMS <democlesvanshepars@gmail.com>
2021-12-29 01:37:50 -0500

    Improve agent detection

--- src/agent.ts
+++ src/agent.ts
@@ -1,20 +1,21 @@
-import BigNumber from "bignumber.js";
 import {
   ethers,
   BlockEvent,
   Finding,
-  HandleBlock,
-  HandleTransaction,
   TransactionEvent,
-  FindingSeverity,
-  FindingType,
   getEthersProvider,
 } from "forta-agent";
 import {
-  APPROVE_SIG,
+  APPROVE_FUNC_SIG,
   APROVAL_EVENT_SIG,
-  INCREASE_ALLOWANCE_SIG,
+  BLOCK_RANGE,
+  INCREASE_ALLOWANCE_FUNC_SIG,
+  TIMES_DETECTED,
+  TRANSFER_FROM_FUNC_SIG,
 } from "./const";
+import EXCHANGES from "./exchanges";
+import { phishingAlert, isZeroAddress } from "./utils";
+
 import erc20Abi from "./erc.abi.json";
 
 const provider = getEthersProvider();
@@ -23,49 +24,94 @@ const provider = getEthersProvider();
 //"7TY68RZP4EI89Z3YYYVVP9WB6TES3G8HAY"
 //);
 
-const suspectsAddress: Map<string, Array<[number, number]>> = new Map();
-
 const iface = new ethers.utils.Interface(erc20Abi);
 
 function provideHandleTransaction(provider: any) {
+  const suspiciousSpenders: Map<string, number[]> = new Map();
+  let lastBlockNumber: number = 0;
+
   return async (txEvent: TransactionEvent) => {
+    console.log("Transaction on block:", txEvent.blockNumber);
     const findings: Finding[] = [];
 
-    let funcCalls = txEvent.filterFunction([
-      APPROVE_SIG,
-      INCREASE_ALLOWANCE_SIG,
+    // Select right events somehow
+    let logs = txEvent.filterFunction([
+      APPROVE_FUNC_SIG,
+      INCREASE_ALLOWANCE_FUNC_SIG,
     ]);
-    const funcCallsn = txEvent.filterLog(APROVAL_EVENT_SIG);
 
-    //console.log(txEvent.traces);
-    if (funcCalls.length === 0) return findings;
+    if (logs.length > 0)
+      console.log(`got logs(${logs.length}):`, logs[0].signature);
 
-    console.log(funcCalls[0]);
+    for (const log of logs) {
+      const spender = log.args.spender;
+      // Do not analyze transactions from known exchanges and smart contracts
+      if (
+        EXCHANGES.has(spender) ||
+        (await provider.getCode(spender)) !== "0x" ||
+        isZeroAddress(spender)
+      ) {
+        console.log("log belongs to contract or exchange");
+        continue;
+      }
 
-    let contract =
-      (await provider.getCode(funcCalls[0].args["spender"])) !== "0x";
-    if (contract) return findings;
+      let transactions = suspiciousSpenders.get(spender);
+      // Block number where suspicious activity ocurred are kept
+      // for the time specified.
+      if (transactions === undefined) {
+        console.log(`for ${spender} adding activity 1`);
+        transactions = [txEvent.blockNumber];
+        suspiciousSpenders.set(spender, transactions);
+      } else {
+        console.log(
+          `for ${spender} adding activity ${transactions.length + 1}`
+        );
+        transactions.push(txEvent.blockNumber);
+      }
 
-    //console.log(txEvent);
-    console.log("Got transaction");
+      if (transactions.length >= TIMES_DETECTED) {
+        console.log("Push findings");
+        findings.push(phishingAlert());
+        // stop tracking this account
+        suspiciousSpenders.delete(spender);
+      }
 
-    for (let funcCall of funcCalls) {
-      console.log("sig", funcCall.signature);
-      console.log("args", funcCall.args);
+      // experimental
+      console.log(log);
+      try {
+        let val = iface.parseTransaction({
+          data: txEvent.transaction.data,
+        });
+        console.log("got func:", val.signature);
+      } catch (error) {
+        console.log("error");
+      }
     }
 
-    try {
-      let val = iface.parseTransaction({
-        data: txEvent.transaction.data,
-      });
-      console.log("got func:", val.signature);
-    } catch (error) {
-      console.log("error");
-      return findings;
+    if (txEvent.blockNumber !== lastBlockNumber) {
+      console.log("Updating last block number and cleaning");
+      lastBlockNumber = txEvent.blockNumber;
+      updateSuspicious(suspiciousSpenders, lastBlockNumber);
     }
 
     return findings;
   };
+}
+
+// In every block delete suspicious activity out of BLOCK_RANGE
+function updateSuspicious(
+  suspiciousSpenders: Map<string, number[]>,
+  blockNumber: number
+) {
+  suspiciousSpenders.forEach((blocks, spender) => {
+    while (blocks[0] - blockNumber > BLOCK_RANGE) {
+      blocks.shift();
+      if (blocks.length === 0) {
+        suspiciousSpenders.delete(spender);
+        return;
+      }
+    }
+  });
 }
 
 export default {
